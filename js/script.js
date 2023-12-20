9d50b6ebd87b85100629538dca00ef58f2cade22
koreamarin <awldnjs2@gmail.com>
2023-12-20 16:28:54 +0900

    update navbar

--- js/script.js
+++ js/script.js
@@ -1,18 +1,27 @@
 (function () {
     // const menu = document.querySelector('.nav-toggle');
-    const navRight = document.querySelector('.nav-right');
-
+    // const navRight = document.querySelector('.nav-right');
     // menu.addEventListener('click', toggleMenu);
-
-    function toggleMenu() {
-        menu
-            .classList
-            .toggle('is-active');
-        navRight
-            .classList
-            .toggle('is-active');
-    }
+    // function toggleMenu() {
+    //     menu
+    //         .classList
+    //         .toggle('is-active');
+    //     navRight
+    //         .classList
+    //         .toggle('is-active');
+    // }
 
     var year = new Date().getFullYear();	// 연도
-    document.getElementById("year").innerText=year
+    document.getElementById("year").innerText = year;
+
+    var urlpath = window.location.pathname.split('/')[1];
+    
+    if (urlpath=="") {
+        document.getElementById('home').classList.add('selected');
+    }
+    else if (urlpath == document.getElementById(urlpath).id) {
+        document.getElementById(urlpath).classList.add('selected');
+    }
+    
+
 })();
